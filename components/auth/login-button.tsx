354742f42dba666c303193b40f6645f0445a41dc
iurych <iury.vibe@gmail.com>
2024-01-08 23:16:22 -0300

    chore: implement landig login page

---
+++ components/auth/login-button.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+interface LoginButtonProps {
+  children: React.ReactNode,
+  mode?: "modal" | "redirect",
+  asChild?: boolean,
+}
+
+export const LoginButton = (
+{
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => 
+
+{
+  const onClick = () => {
+    console.log("LOGIN BUTTON CLICKED")
+  }
+
+  if(mode === "modal" ) {
+
+    return (
+      <span>
+        TODO: implement modal
+      </span>
+    )
+  }
+
+  return(
+    <span onClick={onClick} className="cursor-pointer" >
+      {children}
+    </span>
+  );
+};
