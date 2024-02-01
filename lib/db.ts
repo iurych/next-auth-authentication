2c9cfa58cd4d5717cb5cf0423db364ffb3e73c01
iurych <iury.vibe@gmail.com>
2024-01-31 21:49:20 -0300

    feat: add prima-adapater

---
+++ lib/db.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-var */
+import { PrismaClient } from '@prisma/client'
+
+export const db = globalThis.prisma || new PrismaClient()
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
