72637eebc6cccf7f86655680a437f413334f0bf3
iurych <iury.vibe@gmail.com>
2024-02-17 17:31:11 -0300

    feat: add hash password

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,8 @@
 'use server'
 
+import { db } from '@/lib/db'
 import { RegisterSchema } from '@/schema'
+import bcrypt from 'bcrypt'
 import * as zod from 'zod'
 // equivalent as api route
 
@@ -12,6 +14,29 @@ export const register = async (values: LoginRequest) => {
   if (!validateFields.success) {
     return { error: 'Invalid fields!' }
   }
+
+  const { email, name, password } = validateFields.data
+
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (user) {
+    return { error: 'User already exists!' }
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  })
+
+  //TODO send verification email token
 
   return { success: 'Email sent!' }
 }
